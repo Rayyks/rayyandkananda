a9e97319d0b06a04d42150dd5d81066db893a110
Rayyand <rayydna14@example.com>
2024-02-25 00:39:21 +0700

    update

--- components/Secret.jsx
+++ components/Secret.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { socials } from "@/lib/data";
 import { IoMdClose } from "react-icons/io";
+import { socials } from "@/lib/data";
 
 const Secrets = ({ isClicked, setIsClicked }) => {
   return (
     <AnimatePresence>
       {isClicked && (
         <motion.div
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.8 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="fixed top-0 left-0 right-0 bottom-0 z-[999] flex items-center rounded-3xl justify-center secret-modal"
         >
@@ -18,9 +18,9 @@ const Secrets = ({ isClicked, setIsClicked }) => {
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -50, opacity: 0 }}
-            className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 md:py-14 lg:py-20 mx-auto p-8 rounded-badge shadow-md w-full h-full bg-gradient-to-b from-gray-800 via-gray-900 to-black bg-opacity-80 secret-bg"
+            className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 md:py-14 lg:py-20 mx-auto p-8 rounded-badge shadow-md w-full h-full bg-gradient-to-b from-gray-800 via-gray-900 to-black bg-opacity-80 secret-bg rounded-3xl"
           >
-            <div className="mx-auto h-screen px-4 rounded-3xl bg-black bg-opacity-10 backdrop-filter backdrop-blur-sm">
+            <div className="mx-auto h-screen px-4">
               <div className="flex justify-end">
                 <motion.button
                   initial={{ rotate: 0 }}
@@ -31,6 +31,7 @@ const Secrets = ({ isClicked, setIsClicked }) => {
                   <IoMdClose className="w-14 h-14 transform transition ease-in-out duration-500" />
                 </motion.button>
               </div>
+
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   {/* Connect Button */}
@@ -40,7 +41,7 @@ const Secrets = ({ isClicked, setIsClicked }) => {
                         {socials.map((social, index) => (
                           <a
                             key={index}
-                            className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                            className="bg-blue-600 active:bg-blue-700 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                             href={social.url}
                             target="_blank"
                             rel="noreferrer"
@@ -70,14 +71,14 @@ const Secrets = ({ isClicked, setIsClicked }) => {
 
                 {/* Profile Information */}
                 <div className="text-center mt-12 ">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-white ">
+                  <h3 className="text-5xl font-extrabold leading-normal mb-2 text-white">
                     Rayyand Kananda Syahputra
                   </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                  <div className="text-base leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
                     Batam, Indonesia
                   </div>
-                  <div className="mb-2 text-purple-600 text-xl font-bold">
+                  <div className="mb-4 w-32 mx-auto text-white text-2xl font-bold bg-purple-700 rounded-full p-2">
                     INTJ - A
                   </div>
                 </div>
@@ -86,11 +87,11 @@ const Secrets = ({ isClicked, setIsClicked }) => {
                 <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                        An passionate and self-taught developer with a strong
-                        background in web development and design. I am a fast
-                        learner and a hard worker. I am also a good team player
-                        and a good leader. I am a good problem solver
+                      <p className="mb-6 text-xl leading-relaxed text-blueGray-400">
+                        Passionate and self-taught developer with a strong
+                        background in web development and design. A fast
+                        learner, hard worker, and a problem solver. Welcome to
+                        the future of tech with a touch of INTJ brilliance.
                       </p>
                     </div>
                   </div>
